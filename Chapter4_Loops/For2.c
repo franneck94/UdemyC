c99e8b35e1e971ffcb80dee3af61a5a1b7a48976
Jan S <schaffranek.jan@web.de>
2020-12-05 11:34:48 +0100

    Updated Chapter

--- Chapter4_Loops/For2.c
+++ Chapter4_Loops/For2.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    // 3 * 2 * 4 = 6 * 4 = 24
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 4; i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 3; j++)
         {
-            for (int k = 0; k < 4; k++)
-            {
-                printf("%d/%d/%d!\n", i, j, k);
-            }
+            printf("%d, %d\n", i, j);
         }
     }
 
--- Chapter4_Loops/SolutionChapter4.c
+++ Chapter4_Loops/SolutionChapter4.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main()
 {
@@ -8,15 +8,25 @@ int main()
         for (int j = 0; j < 6; j++)
         {
             if (j == 0)
+            {
                 printf("<\t");
+            }
             else if (j == 5)
+            {
                 printf(">\t");
+            }
             else if (i == 0)
+            {
                 printf("  ^\t");
+            }
             else if (i == 5)
+            {
                 printf("  v\t");
+            }
             else
+            {
                 printf("(%d,%d)\t", i, j);
+            }
         }
 
         printf("\n");
