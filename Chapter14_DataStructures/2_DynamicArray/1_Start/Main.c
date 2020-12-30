d6fac613efb5b4a5639f926755fbef613748ab72
Jan S <schaffranek.jan@web.de>
2020-12-30 11:27:47 +0100

    Updated Videos

---
+++ Chapter14_DataStructures/2_DynamicArray/1_Start/Main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "DynamicArray.h"
+
+int main()
+{
+    
+    return 0;
+}
---
+++ Chapter14_DataStructures/2_DynamicArray/2_Functions/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "DynamicArray.h"
+
+int main()
+{
+    dynamic_array_t *dynamic_array = createDynamicArray();
+
+
+    return 0;
+}
