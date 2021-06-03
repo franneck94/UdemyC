363cb1fe4d61182a82d88b9251c9b8c6acb56216
Jan S <schaffranek.jan@web.de>
2021-06-03 23:31:43 +0200

    Remove ignored files

--- Chapter16_SimpleCSVReader/Main.c
+++ Chapter16_SimpleCSVReader/Main.c
@@ -7,15 +7,38 @@
 #define FOLDER_LEN 128
 #define FILE_LEN 64
 
-const char PROJECT_PATH[FOLDER_LEN] = "C:/Users/Jan/Dropbox/_Coding/UdemyC/Chapter16_SimpleCSVReader/";
-
-int main()
+// MainDebug.exe "data_in.csv" "data_out2.csv"
+int main(int argc, char **argv)
 {
-    records_t *my_records = create_records();
+    // IO Code
 
+    char project_path[FOLDER_LEN] = { '\0' };
+    char input_filename[FILE_LEN] = { '\0' };
     char input_filepath[FOLDER_LEN + FILE_LEN] = { '\0' };
-    strncpy(input_filepath, PROJECT_PATH, FOLDER_LEN);
-    strncat(input_filepath, "data_in.csv", FILE_LEN);
+    char output_filename[FILE_LEN] = { '\0' };
+    char output_filepath[FOLDER_LEN + FILE_LEN] = { '\0' };
+
+    if (argc == 4)
+    {
+        strncpy(project_path, argv[1], FOLDER_LEN);
+        strncpy(input_filename, argv[2], FILE_LEN);
+        strncpy(output_filename, argv[3], FILE_LEN);
+    }
+    else
+    {
+        strncpy(project_path, "C:/Users/Jan/Dropbox/_Coding/UdemyC/Chapter16_SimpleCSVReader/", FOLDER_LEN);
+        strncpy(input_filename, "data_in.csv", FILE_LEN);
+        strncpy(output_filename, "data_out.csv", FILE_LEN);
+    }
+
+    strncpy(input_filepath, project_path, FOLDER_LEN);
+    strncpy(input_filepath, input_filename, FILE_LEN);
+    strncpy(output_filepath, project_path, FOLDER_LEN);
+    strncpy(output_filepath, output_filename, FILE_LEN);
+
+    // CSV Functions
+
+    records_t *my_records = create_records();
 
     RETURN_CODES return_code = read_simple_csv(input_filepath, my_records);
 
@@ -36,9 +59,6 @@ int main()
     sort_records(my_records, SORT_DESCENDING);
     print_records(my_records, "Descending Sort");
 
-    char output_filepath[FOLDER_LEN + FILE_LEN] = { '\0' };
-    strncpy(output_filepath, PROJECT_PATH, FOLDER_LEN);
-    strncat(output_filepath, "data_out.csv", FILE_LEN);
 
     return_code = write_simple_csv(output_filepath, my_records);
 
