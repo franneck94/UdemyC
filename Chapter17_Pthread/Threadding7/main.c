d58ce2c6f77a85b52c8fa4d5b3275d673de5fcdf
franneck94 <schaffranek.jan@web.de>
2022-01-22 11:32:08 +0100

    up

--- Chapter17_Pthread/Threadding7/main.c
+++ Chapter17_Pthread/Threadding7/main.c
@@ -1,73 +1,58 @@
-#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <pthread.h>
 
-#define NUM_THREADS 10
-#define NUM_ITERATIONS 100000
-#define USE_MUTEX
-#define EXPECTED_RESULT (NUM_THREADS * NUM_ITERATIONS)
+#define NUM_THREADS 3
 
-int32_t global_counter = 0;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+typedef struct
+{
+    int32_t input1;
+    int32_t input2;
+    int32_t output;
+} ThreadData_t;
 
 void *function(void *args)
 {
-    const int32_t arg_i32 = *((int *)(args));
+    ThreadData_t *data = (ThreadData_t *)(args);
 
-    printf("Argument: %d\n", arg_i32);
+    printf("Argument: %d, %d\n", data->input1, data->input2);
 
-    int *result = (int *)malloc(sizeof(int));
-    *result = arg_i32 * 2;
+    data->output = data->input1 + data->input2;
 
-    int32_t local_counter = 0;
-
-    for (int32_t i = 0; i < NUM_ITERATIONS; ++i)
-    {
-        ++local_counter;
-    }
-
-    pthread_mutex_lock(&mutex);
-    global_counter += local_counter;
-    pthread_mutex_unlock(&mutex);
-
-    pthread_exit((void *)(result));
+    return NULL;
 }
 
 int main()
 {
     pthread_t threads[NUM_THREADS];
 
-    int32_t inputs[NUM_THREADS];
-    int32_t *results[NUM_THREADS];
+    ThreadData_t thread_data[NUM_THREADS];
 
     for (int32_t i = 0; i < NUM_THREADS; ++i)
     {
-        inputs[i] = i;
+        thread_data[i].input1 = 1 * i;
+        thread_data[i].input2 = 2 * i;
+        thread_data[i].output = 0;
     }
 
     for (int32_t i = 0; i < NUM_THREADS; ++i)
     {
-        pthread_create(&threads[i], NULL, &function, (void *)(&inputs[i]));
+        pthread_create(&threads[i], NULL, &function, (void *)(&thread_data[i]));
     }
 
     // ...
 
     for (int32_t i = 0; i < NUM_THREADS; ++i)
     {
-        pthread_join(threads[i], (void *)(&results[i]));
+        pthread_join(threads[i], NULL);
     }
 
     for (int32_t i = 0; i < NUM_THREADS; ++i)
     {
-        printf("Result1: %d\n", *results[i]);
+        printf("Result: %d\n", thread_data[i].output);
     }
-
-    printf("Counter: %d\n", global_counter);
-
-    assert(EXPECTED_RESULT == global_counter);
 
     return 0;
 }
