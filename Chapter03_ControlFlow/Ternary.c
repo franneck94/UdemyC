25edf9ef1709586ab97b27a87daf342bcd0c25c4
franneck94 <schaffranek.jan@web.de>
2022-06-20 16:08:52 +0200

    up

--- Chapter03_ControlFlow/Ternary.c
+++ Chapter03_ControlFlow/Ternary.c
@@ -5,17 +5,16 @@ int main()
     int age1 = 28;
     int age2 = 56;
 
-    // Standard If
+    // 1.
     int older_age1;
     if (age1 > age2)
         older_age1 = age1;
     else
         older_age1 = age2;
-    printf("older_age1: %d\n", older_age1);
 
-    // Ternary
+    // 2. Ternary Operator
+    //               BOOL EXPR   ? True : False
     int older_age2 = age1 > age2 ? age1 : age2;
-    printf("older_age2: %d\n", older_age2);
 
     return 0;
 }
