4b9b0c4c5a44669824bf856b0e2c9bef6f9ecbd8
franneck94 <schaffranek.jan@web.de>
2022-08-22 19:57:23 +0200

    up

--- Chapter12_Advanced1/Timing/Main.c
+++ Chapter12_Advanced1/Timing/Main.c
@@ -5,9 +5,9 @@ void do_something()
 {
     volatile double value = 0.0;
 
-    for (int i = 0; i < 10000000; i++)
+    for (unsigned int i = 0; i < 100000000; i++)
     {
-        value *= i;
+        value += i;
     }
 }
 
@@ -20,15 +20,15 @@ int main()
     clock_t time_end = clock();
     clock_t duration = time_end - time_start;
 
-    double s = ((double)duration) / CLOCKS_PER_SEC;
-    double ms = 1000.0 * s;   // s * 10^3
-    double mus = 1000.0 * ms; // s * 10^6
-    double ns = 1000.0 * mus; // s * 10^9
+    double s = (double)duration / CLOCKS_PER_SEC;
+    double ms = 1000.0 * s;
+    double us = 1000.0 * ms;
+    double ns = 1000.0 * us;
 
-    printf("Seconds: %lf\n", s);
-    printf("Milli-Seconds: %lf\n", ms);
-    printf("Mirco-Seconds: %lf\n", mus);
-    printf("Nano-Seconds: %lf\n", ns);
+    printf("s: %lf\n", s);
+    printf("ms: %lf\n", ms);
+    printf("us: %lf\n", us);
+    printf("ns: %lf\n", ns);
 
     return 0;
 }
